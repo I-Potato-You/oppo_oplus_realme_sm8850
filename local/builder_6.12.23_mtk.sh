237fb488c4f36c7fe7fa81433821ea5f9996955d
南城旧梦 <81269358+I-Potato-You@users.noreply.github.com>
2026-04-02 22:17:08 +0800

    Update builder_6.12.23_mtk.sh

--- local/builder_6.12.23_mtk.sh
+++ local/builder_6.12.23_mtk.sh
@@ -181,7 +181,7 @@ if [[ "$KSU_BRANCH" == "y" || "$KSU_BRANCH" == "Y" ]]; then
   grep -A10 "REPO_OWNER" kernel/Kbuild | head -n 10
   echo "------------------------------------------------"
   grep "KSU_VERSION_FULL" kernel/Kbuild
-  echo ">>> 最终版本字符串: v${KSU_API_VERSION}-${GIT_COMMIT_HASH}@cctv18"
+  echo ">>> 最终版本字符串: v${KSU_API_VERSION}-@南城定制内核"
   echo ">>> Version Code: ${KSU_VERSION_CODE}"
 elif [[ "$KSU_BRANCH" == "r" || "$KSU_BRANCH" == "R" ]]; then
   echo ">>> 拉取 ReSukiSU 并设置版本..."
